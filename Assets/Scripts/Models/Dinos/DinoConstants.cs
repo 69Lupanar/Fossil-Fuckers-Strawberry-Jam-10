f348aae5b53ea8c9e41e5ede886396ea4399f59c
Lupanar <69lupanar@proton.me>
2026-02-05 10:14:55 +0100

    ajout SO des luxurosaures

---
+++ Assets/Scripts/Models/Dinos/DinoConstants.cs
@@ -0,0 +1,19 @@
+namespace Assets.Scripts.Models.Dinos
+{
+    /// <summary>
+    /// Constantes relatives aux luxurosaures
+    /// </summary>
+    public static class DinoConstants
+    {
+        /// <summary>
+        /// Le niveau max que peut atteindre un luxurosaure
+        /// </summary>
+        public const int MAX_LEVEL = 10;
+
+        /// <summary>
+        /// Le max d'EXP que le luxurosaure devra obtenir pour atteindre le dernier niveau
+        /// </summary>
+        public const int TOTAL_EXP = 5000;
+
+    }
+}
--- Assets/Scripts/Models/Dinos/LustosaurSO.cs
+++ Assets/Scripts/Models/Dinos/LustosaurSO.cs
@@ -8,6 +8,27 @@ namespace Assets.Scripts.Models.Dinos
     [CreateAssetMenu(fileName = "Lustosaur", menuName = "Scriptable Objects/Lustosaur")]
     public class LustosaurSO : ScriptableObject
     {
+        #region Propriétés
+
+        /// <summary>
+        /// Le montant d'exp actuel du luxurosaure
+        /// </summary>
+        public int CurEXP { get; set; } = 0;
+
+        /// <summary>
+        /// Le niveau actuel du luxurosaure
+        /// </summary>
+        public int CurLevel { get; set; } = 1;
+
+        /// <summary>
+        /// L'exp à gagner pour atteindre le niveau suivant
+        /// </summary>
+        public int ExpUntilNextLevel => Mathf.RoundToInt(EXPProgressCurve.Evaluate((float)(CurLevel) / (float)DinoConstants.MAX_LEVEL - 1) * DinoConstants.TOTAL_EXP);
+
+        #endregion
+
+        #region Variables Unity
+
         /// <summary>
         /// L'attribut du luxurosaure
         /// </summary>
@@ -32,5 +53,47 @@ namespace Assets.Scripts.Models.Dinos
         [field: SerializeField]
         public FightingStats SupportStats { get; private set; }
 
+        /// <summary>
+        /// Courbe de progression de gaind d'EXP au fil des niveaux
+        /// </summary>
+        [field: SerializeField]
+        public AnimationCurve EXPProgressCurve { get; private set; }
+
+        #endregion
+
+        #region Méthodes publiques
+
+        /// <summary>
+        /// Gagne de l'expérience pour monter au niveau suivant
+        /// </summary>
+        /// <param name="amount">Le montant</param>
+        public void GainEXP(int amount)
+        {
+            if (CurLevel == DinoConstants.MAX_LEVEL)
+            {
+                return;
+            }
+
+            CurEXP += amount;
+
+            if (CurEXP > ExpUntilNextLevel)
+            {
+                GainLevel();
+            }
+        }
+
+        /// <summary>
+        /// Gagne un niveau et réajuste l'EXP restant
+        /// </summary>
+        public void GainLevel()
+        {
+            if (CurLevel < DinoConstants.MAX_LEVEL)
+            {
+                ++CurLevel;
+                CurEXP -= CurLevel == DinoConstants.MAX_LEVEL ? CurEXP : ExpUntilNextLevel;
+            }
+        }
+
+        #endregion
     }
 }
